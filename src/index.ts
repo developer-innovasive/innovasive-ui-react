ccf450d3b15b28469166412d905a2c1e43213f55
chanonpingpong <chanon.pingpong@gmail.com>
2020-10-15 11:13:47 +0700

    🏓 fix: some components don't include in index.ts

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,36 @@ export * from './components/ButtonSecondary'
 export * from './components/ButtonIcon'
 /**
 |--------------------------------------------------
+| MENU
+|--------------------------------------------------
+*/
+export * from './components/Menu'
+/**
+|--------------------------------------------------
+| SWITCH
+|--------------------------------------------------
+*/
+export * from './components/Switch'
+/**
+|--------------------------------------------------
 | TABLE
 |--------------------------------------------------
 */
 export * from './components/DataTable'
+/**
+|--------------------------------------------------
+| MODAL
+|--------------------------------------------------
+*/
+export * from './components/ModalBase'
+export * from './components/ModalConfirm'
+/**
+|--------------------------------------------------
+| LOADING
+|--------------------------------------------------
+*/
+export * from './components/Loading'
+export * from './components/DarkLoading'
 /**
 |--------------------------------------------------
 | HELPERS
